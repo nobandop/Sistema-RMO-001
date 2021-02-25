825a69996b298c312c8a9926ab9643db12f1ba21
nobandop <78871962+nobandop@users.noreply.github.com>
2021-02-25 13:02:23 -0500

    Creación del código fuente

---
+++ Sistema RMO-001/include/Proveedor.h
@@ -0,0 +1,15 @@
+#ifndef PROVEEDOR_H
+#define PROVEEDOR_H
+
+
+class Proveedor
+{
+    public:
+        Proveedor();
+
+    protected:
+
+    private:
+};
+
+#endif // PROVEEDOR_H
---
+++ Sistema RMO-001/src/Proveedor.cpp
@@ -0,0 +1,6 @@
+#include "Proveedor.h"
+
+Proveedor::Proveedor()
+{
+    //ctor
+}
